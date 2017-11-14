f70bf449a3c53ea2f42056f9b29615063a69dfa8
Saitama7 <djkaira7@gmail.com>
2017-11-14 23:03:23 +0600

    new product form NOT finished

--- resources/views/modals/product.blade.php
+++ resources/views/modals/product.blade.php
@@ -12,29 +12,30 @@
                 {{ csrf_field() }}
                 <div class="modal-body">
                     <div class="row pt-3">
-                        <div class="col">
-                            <input type="text" name="name" class="form-control" placeholder="Наименование" size="25">
+                        <div class="col-6">
+                            <label class="pull-left">Наименование</label>
+                            <input type="text" name="name" class="form-control pull" placeholder="Наименование" size="25">
                         </div>
-                        <div class="col">
+                        <div class="col-6">
                             <input type="text" name="desc" class="form-control" placeholder="Описание" size="25">
                         </div>
-                        <div class="col">
+                        <div class="col-6">
                             <input type="text" name="cost" class="form-control" placeholder="Цена" size="10">
                         </div>
-                        <div class="col">
-                            <input type="text" name="type_id" class="form-control" placeholder="Цена" size="10">
+                        <div class="col-6">
+                            <input type="text" name="type_id" class="form-control" placeholder="Тип" size="10">
                         </div>
-                        <div class="col">
-                            <input type="text" name="size_id" class="form-control" placeholder="Цена" size="10">
+                        <div class="col-6">
+                            <input type="text" name="size_id" class="form-control" placeholder="Размер" size="10">
                         </div>
-                        <div class="col">
-                            <input type="text" name="image_path" class="form-control" placeholder="Цена" size="10">
+                        <div class="col-6">
+                            <input type="text" name="image_path" class="form-control" placeholder="Картина" size="10">
                         </div>
-                        <div class="col">
-                            <input type="text" name="status" class="form-control" placeholder="Цена" size="10">
+                        <div class="col-6">
+                            <input type="text" name="status" class="form-control" placeholder="Видимость" size="10">
                         </div>
-                        <div class="col">
-                            <input type="text" name="slide_status" class="form-control" placeholder="Цена" size="10">
+                        <div class="col-6">
+                            <input type="text" name="slide_status" class="form-control" placeholder="Показать на слайде" size="10">
                         </div>
 
                     </div>
