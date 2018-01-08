7661bfdabd25d5d019e22b584e1cc271e22b4496
Saitama7 <djkaira7@gmail.com>
2018-01-08 12:45:53 +0600

    1.7.1

--- local/resources/views/layouts/admin.blade.php
+++ local/resources/views/layouts/admin.blade.php
@@ -37,6 +37,7 @@
                         {{--<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">--}}
                         {{--<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>--}}
                     {{--</form>--}}
+                    <div style="width: 1200px; height: 50px"></div>
 
                 </div>
 
